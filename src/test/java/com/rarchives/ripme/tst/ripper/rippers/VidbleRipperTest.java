17a17bdbbaf3568a60887a3c3aa8fc6880e85d5b
Stefan Aladzic <69090391+saladzic@users.noreply.github.com>
2021-11-03 23:57:32 +0100

    Disabling Vidble Unit Test as the website requires authorization to work correctly Issue: https://github.com/ripmeapp2/ripme/issues/51

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/VidbleRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/VidbleRipperTest.java
@@ -5,16 +5,20 @@ import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.VidbleRipper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class VidbleRipperTest extends RippersTest {
     @Test
+    @Disabled("https://github.com/ripmeapp2/ripme/issues/51")
     public void testVidbleRip() throws IOException {
         VidbleRipper ripper = new VidbleRipper(new URL("http://www.vidble.com/album/y1oyh3zd"));
         testRipper(ripper);
     }
 
     @Test
+    @Disabled("https://github.com/ripmeapp2/ripme/issues/51")
     public void testGetGID() throws IOException {
         URL url = new URL("http://www.vidble.com/album/y1oyh3zd");
         VidbleRipper ripper = new VidbleRipper(url);
