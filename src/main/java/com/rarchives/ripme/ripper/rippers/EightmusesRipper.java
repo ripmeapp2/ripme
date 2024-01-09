0eaf9c2684e419a874249eeef88cc943f87ff358
joroto <obsoleteserenity@gmail.com>
2024-01-10 01:05:36 +0200

    Refactoring

--- src/main/java/com/rarchives/ripme/ripper/rippers/EightmusesRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/EightmusesRipper.java
@@ -10,7 +10,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.jsoup.Connection.Response;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
